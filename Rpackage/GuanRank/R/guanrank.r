50c2d321d2298f8d2e437c90dcdcd99ed987f4f2
Chen <verne91@gmail.com>
2018-12-01 23:31:34 -0500

    formal package

---
+++ Rpackage/GuanRank/R/guanrank.r
@@ -0,0 +1,9 @@
+dyn.load(paste("GuanRank", .Platform$dynlib.ext, sep=""))
+guanrank <- function(infile){
+  df <- read.csv(infile)
+  n <- nrow(df)
+
+  res <- .C("guanrank", infile, rank=double(n), pid=character(n))
+
+  return (data.frame(res$pid, res$rank))
+}
